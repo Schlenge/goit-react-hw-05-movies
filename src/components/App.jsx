d09ad9218a1b570306ef52b51d231b1386cfa687
Schlenge <emil.tarala@gmail.com>
2023-11-27 20:13:09 +0200

    commit 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,28 @@
-import Styled from '../components/App.module.css';
-
-const StyledLink = styled(NavLink)`
-  color: black;
-
-  &.active {
-    color: orange;
-  }
-`;
+import React from 'react';
+//import { Route } from 'react-router-dom';
+//import Styled from '../components/App.module.css';
 
 export const App = () => {
   return (
     <div>
       <h1>React HM-5 Movies</h1>
+      {/*
       <nav>
         <StyledLink to="/" end>
           Home
         </StyledLink>
-        <StyledLink to="/trending">Trending</StyledLink>
-        <StyledLink to="/search">Search</StyledLink>
         <StyledLink to="/movies">Movies</StyledLink>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<Trending />} />
-        <Route path="/products" element={<Search />} />
-        <Route path="/products" element={<Movies />} />
+        <Route path="/movies" element={<Movies />}>
+          <Route path="/movies/:movieId" element={<MovieId />}>
+            <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+          </Route>
+        </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
+  */}
     </div>
   );
 };
---
+++ src/components/Movies/Movies.jsx
@@ -0,0 +1,1 @@
+import css from './Movies.module.css';
