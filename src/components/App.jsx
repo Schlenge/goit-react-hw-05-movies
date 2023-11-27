c0a67870abfe66b2cd071f7a06062c10714e7cf6
Schlenge <emil.tarala@gmail.com>
2023-11-27 19:57:53 +0200

    commit 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
+import Styled from '../components/App.module.css';
+
+const StyledLink = styled(NavLink)`
+  color: black;
+
+  &.active {
+    color: orange;
+  }
+`;
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>React HM-5 Movies</h1>
+      <nav>
+        <StyledLink to="/" end>
+          Home
+        </StyledLink>
+        <StyledLink to="/trending">Trending</StyledLink>
+        <StyledLink to="/search">Search</StyledLink>
+        <StyledLink to="/movies">Movies</StyledLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<Trending />} />
+        <Route path="/products" element={<Search />} />
+        <Route path="/products" element={<Movies />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
