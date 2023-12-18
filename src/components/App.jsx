f235edd0a7119ff3c9d016ac017011996dda9af5
Schlenge <emil.tarala@gmail.com>
2023-12-18 23:41:11 +0200

    commit 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,25 @@
-import React from 'react';
-//import { Route } from 'react-router-dom';
-//import Styled from '../components/App.module.css';
+import { lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { SharedLayout } from './SharedLayout/SharedLayout';
+import { Cast } from './Cast/Cast';
+import { Reviews } from './Reviews/Reviews';
+
+const Home = lazy(() => import('../Pages/Home'));
+const Movies = lazy(() => import('../Pages/Movies'));
+const MovieDetails = lazy(() => import('../Pages/MovieDetails'));
 
 export const App = () => {
   return (
-    <div>
-      <h1>React HM-5 Movies</h1>
-      {/*
-      <nav>
-        <StyledLink to="/" end>
-          Home
-        </StyledLink>
-        <StyledLink to="/movies">Movies</StyledLink>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />}>
-          <Route path="/movies/:movieId" element={<MovieId />}>
-            <Route path="/movies/:movieId/reviews" element={<Reviews />} />
-          </Route>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
         </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-  */}
-    </div>
+        <Route path="*" element={<Home />} />
+      </Route>
+    </Routes>
   );
 };
--- src/components/Movies/Movies.jsx
+++
@@ -1,1 +0,0 @@
-import css from './Movies.module.css';
